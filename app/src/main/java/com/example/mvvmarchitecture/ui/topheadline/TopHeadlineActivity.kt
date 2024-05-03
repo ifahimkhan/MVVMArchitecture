78c87117c8c416a7473864049ac6ff630200454e
ifahimkhan <khan.fahim95@hotmail.com>
2024-05-04 00:11:09 +0530

    if applied this commit will complete Ui layer

--- app/src/main/java/com/example/mvvmarchitecture/ui/topheadline/TopHeadlineActivity.kt
+++ app/src/main/java/com/example/mvvmarchitecture/ui/topheadline/TopHeadlineActivity.kt
@@ -1,12 +1,82 @@
 package com.example.mvvmarchitecture.ui.topheadline
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle.*
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.manager.Lifecycle
+import com.example.mvvmarchitecture.MVVMApplication
 import com.example.mvvmarchitecture.R
+import com.example.mvvmarchitecture.data.model.Article
+import com.example.mvvmarchitecture.databinding.ActivityTopHeadlineBinding
+import com.example.mvvmarchitecture.di.module.ActivityModule
+import com.example.mvvmarchitecture.ui.base.UiState
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class TopHeadlineActivity : AppCompatActivity() {
+    @Inject
+    lateinit var topHeadlineViewModel: TopHeadlineViewModel
+    @Inject
+    lateinit var adapter: TopHeadlineAdapter
+    private lateinit var binding: ActivityTopHeadlineBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        injectDependencies()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_top_headline)
+        binding = ActivityTopHeadlineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUI()
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        lifecycleScope.launch{
+            repeatOnLifecycle(State.STARTED){
+                topHeadlineViewModel.uiState.collect{
+                    when(it){
+                        is UiState.Success ->{
+                            binding.progressBar.visibility = View.GONE
+                            renderList(it.data)
+                            binding.recyclerView.visibility = View.VISIBLE
+                        }
+                        is UiState.Error ->{
+                            binding.progressBar.visibility = View.GONE
+                            Toast.makeText(this@TopHeadlineActivity,it.message,Toast.LENGTH_LONG).show()
+                        }
+
+                        UiState.Loading -> {
+                            binding.progressBar.visibility = View.VISIBLE
+                            binding.recyclerView.visibility = View.GONE
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun renderList(data: List<Article>) {
+        adapter.addData(data)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupUI() {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context,
+            (recyclerView.layoutManager as LinearLayoutManager).orientation))
+        recyclerView.adapter = adapter
+    }
+    private fun injectDependencies(){
+        DaggerActivityComponent.builder()
+            .applicationComponent((application as MVVMApplication).applicationComponent)
+            .activityModule(ActivityModule(this))
+            .build().inject(this)
     }
 }
