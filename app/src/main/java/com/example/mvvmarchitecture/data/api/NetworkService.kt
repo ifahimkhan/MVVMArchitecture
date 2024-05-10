a54a3b7806e2d821719b26755199fb13ef7088b7
ifahimkhan <khan.fahim95@hotmail.com>
2024-05-10 23:51:50 +0530

    if applied this commit will Add network services for source and country

--- app/src/main/java/com/example/mvvmarchitecture/data/api/NetworkService.kt
+++ app/src/main/java/com/example/mvvmarchitecture/data/api/NetworkService.kt
@@ -1,5 +1,6 @@
 package com.example.mvvmarchitecture.data.api
 
+import com.example.mvvmarchitecture.data.model.NewsSourceResponse
 import com.example.mvvmarchitecture.data.model.TopHeadlineResponse
 import com.example.mvvmarchitecture.utils.AppConstant.API_KEY
 import retrofit2.http.GET
@@ -12,4 +13,13 @@ interface NetworkService {
     @Headers("X-Api-Key: $API_KEY")
     @GET("top-headlines")
     suspend fun getTopHeadlines(@Query("country") country: String): TopHeadlineResponse
+
+    @Headers("X-Api-Key: $API_KEY")
+    @GET("top-headlines")
+    suspend fun getNewsByCountry(@Query("country") country: String): TopHeadlineResponse
+
+    @Headers("X-Api-Key: $API_KEY")
+    @GET("top-headlines")
+    suspend fun getNewsBySources(@Query("sources") source: String): TopHeadlineResponse
+
 }
--- app/src/main/java/com/example/mvvmarchitecture/utils/AppConstant.kt
+++ app/src/main/java/com/example/mvvmarchitecture/utils/AppConstant.kt
@@ -1,8 +1,21 @@
 package com.example.mvvmarchitecture.utils
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 object AppConstant {
 
     const val API_KEY = "9f6482a584804376874b848980b7a044"
     const val COUNTRY = "us"
 
+    sealed class NewsType : Parcelable {
+        @Parcelize
+        data class COUNTRY(var code: String) : NewsType()
+
+        @Parcelize
+        data class SOURCE(var id: String) : NewsType()
+
+        @Parcelize
+        data class LANGUAGE(var language: String) : NewsType()
+    }
 }
