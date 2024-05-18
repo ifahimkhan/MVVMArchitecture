12f6e6fad2ff35ea2ad8a6e003516acfb548914f
ifahimkhan <khan.fahim95@hotmail.com>
2024-05-18 21:37:44 +0530

    if applied this commit will modify dagger to Hilt

--- app/src/main/java/com/example/mvvmarchitecture/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/mvvmarchitecture/ui/base/BaseActivity.kt
@@ -2,7 +2,7 @@ package com.example.mvvmarchitecture.ui.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mvvmarchitecture.utils.Logger
+import com.example.mvvmarchitecture.utils.logger.Logger
 import javax.inject.Inject
 
 open class BaseActivity : AppCompatActivity() {
